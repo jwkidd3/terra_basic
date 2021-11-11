6191cd94b31ea1dada3e76da959c797b00d5bef9
EC2 Default User <ec2-user@ip-172-31-75-91.ec2.internal>
2021-11-11 08:30:47 +0000

    initial

---
+++ mod/mod.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_iam_user" "classuser" {
+  name = "user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user_login_profile" "userprof" {
+  user    = aws_iam_user.classuser.name
+  pgp_key = "keybase:some_person_that_exists"
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.userprof.encrypted_password
+}
